352207f42a9354d1299a3cb24ca1058ab0f101ab
Gregori Maia <gregorimaia@gmail.com>
2023-09-29 23:38:59 -0300

    Primeiro commit do Flow

---
+++ app/views/flows/_flow.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! flow, :id, :item, :value, :created_at, :updated_at
+json.url flow_url(flow, format: :json)
---
+++ db/migrate/20230930014015_create_flows.rb
@@ -0,0 +1,10 @@
+class CreateFlows < ActiveRecord::Migration[7.0]
+  def change
+    create_table :flows do |t|
+      t.string :item
+      t.decimal :value
+
+      t.timestamps
+    end
+  end
+end
