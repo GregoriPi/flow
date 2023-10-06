8acb6357d63fc352c4ae74692c1de3d8c371c81b
Gregori Maia <gregorimaia@gmail.com>
2023-10-05 23:46:41 -0300

    User's password and authentication.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_204407) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_06_023329) do
   create_table "flows", force: :cascade do |t|
     t.string "item"
     t.decimal "value"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_204407) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
